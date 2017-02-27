d9b9bd41f56f8423dd0e307d17f1e0e390c464c8
Home <gehrt.patrick@outlook.com>
2017-02-27 18:10:59 +0100

    Projektdateien hinzufügen.

---
+++ SFML_GD_Pong/Source/Main.cpp
@@ -0,0 +1,25 @@
+/**********************************************************************************************************************************
+Title:                Snake
+
+Erstellungsdatum:     27.02.2017
+letzte Änderung:      27.02.2017
+
+Author:               Patrick Gehrt
+Beschreibung:
+  Snake-Game auf Basis des SFML-Frameworks
+
+SFML-Version:         2.4.2
+Zielplattform:        32Bit, x86, Win
+
+App-Version:          0.0.0.1
+**********************************************************************************************************************************/
+
+#include <iostream>
+
+int main(int argc, char* argv[] )
+{
+  
+
+
+  return EXIT_SUCCESS;
+}
